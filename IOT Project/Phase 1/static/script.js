e1bb0944a21bae922776841f81d616a1d5a6c52a
AmirGeorgesHaya <129766673+AmirGeorgesHaya@users.noreply.github.com>
2025-02-28 11:03:57 -0500

    Add files via upload

--- IOT Project/Phase 1/static/script.js
+++ IOT Project/Phase 1/static/script.js
@@ -2,25 +2,30 @@
 let ledStatus = 'OFF';
 
 //if button is clicked:
-document.getElementById('toggle-button').addEventListener('click', () => {
+document.getElementById('switch-image').addEventListener('click', () => {
     // Toggle the switch status. it checks what the status is and does the opposite
     //if its off makes it on
     if (ledStatus === 'OFF') {
         ledStatus = 'ON';
+        document.getElementById('switch-image').src = '../static/onswitch.png';
     } 
     //if on make it off
     else {
         ledStatus = 'OFF';
+        document.getElementById('switch-image').src = '../static/offswitch.png';
     }            
     //Change the  switch status to whatever the led status is on or off.
     // ex: Switch Status: OFF/ON
     document.getElementById('switch-status').textContent = ledStatus;
+
     //change the LED COLOR ON SCREEN
     document.getElementById('led').className = ledStatus.toLowerCase();
     
     // Send a request to the Flask backend to update the LED status
-    fetch(`/toggle_led?status=${ledStatus}`).then(response => response.json())
+    fetch(`/toggle_led?status=${ledStatus}`)
+        .then(response => response.json())
         .then(data => {
             console.log(data.message);
-        });
+        })
+        .catch(error => console.error('Error:', error));
 });
